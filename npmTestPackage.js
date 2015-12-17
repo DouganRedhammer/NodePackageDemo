19be225cc55e663f1af05bb46d4cde858e99753e
unknown <dfranklin@DFRANKLIN-WUB.psisystems.local>
2015-12-17 14:49:25 -0800

    init

---
+++ npmTestPackage.js
@@ -0,0 +1,11 @@
+module.exports = {  
+  add: function(a, b){
+	  return a + b;
+  },
+  subtract: function(a,b){
+	  return a - b;
+  },
+  sayHello: function(name){
+	  return "Hello " + name; 
+  }
+};
